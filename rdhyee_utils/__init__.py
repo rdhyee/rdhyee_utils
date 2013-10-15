5e6c5b24aa09f63dc17913d9480a57a173f37c3a
Raymond Yee <raymond.yee@gmail.com>
2013-10-15 10:11:46 -0700

    First commit

---
+++ rdhyee_utils/__init__.py
@@ -0,0 +1,24 @@
+# http://stackoverflow.com/questions/2348317/how-to-write-a-pager-for-python-iterators/2350904#2350904        
+def grouper(iterable, page_size):
+    page= []
+    for item in iterable:
+        page.append( item )
+        if len(page) == page_size:
+            yield page
+            page= []
+    if len(page) > 0:
+        yield page
+
+#http://stackoverflow.com/questions/42558/python-and-the-singleton-pattern/2752280#2752280
+def singleton(cls):
+    instances = {}
+    def getinstance():
+        if cls not in instances:
+            instances[cls] = cls()
+        return instances[cls]
+    return getinstance
+
+def nowish_tz():
+    # put in Pacific time
+    tz_PT = pytz.timezone("US/Pacific")
+    return datetime.datetime(*datetime.datetime.utcnow().timetuple()[:6]).replace(tzinfo=pytz.utc).astimezone(tz_PT)
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='rdhyee_utils',
+      version=version,
+      description="Some simple methods and classes Raymond Yee developed for his own work.",
+      long_description="""\
+Some simple methods and classes Raymond Yee developed for his own work.""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='utilities',
+      author='Raymond Yee',
+      author_email='raymond.yee@gmail.com',
+      url='',
+      license='Apache 2.0',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
